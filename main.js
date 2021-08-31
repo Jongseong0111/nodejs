0b4854156eff3884a88bac4a6afa6b1639d0152a
임종성 <qkqndudnxld@gmail.com>
2021-08-31 10:51:39 +0900

    Node.js - 15

--- main.js
+++ main.js
@@ -5,39 +5,39 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     let queryData = url.parse(_url, true).query;
+    let pathname = url.parse(_url, true).pathname;
     let title = queryData.id;
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-      response.writeHead(404);
-      response.end();
-      return;
-    }
-    response.writeHead(200);
-    fs.readFile(`nodejs/data/${queryData.id}`, 'utf8', function(err, data) {
-      var description = data;
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
-      response.end(template);
-    })
+    
+    console.log(url.parse(_url, true))
+
+    if(pathname === '/'){
+      fs.readFile(`nodejs/data/${queryData.id}`, 'utf8', function(err, data) {
+        var description = data;
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ol>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ol>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.end(template);
+      });
+  } else {
+    response.writeHead(404);
+    response.end('page not found');
+  }
     
  
 });
